61194b4a3cb8196222b4527fe0c6be380c60eb66
rcpeene@gmail.com <rcpeene@gmail.com>
2022-04-02 12:24:13 -0700

    Added skeleton of test code

--- test/Test.py
+++ test/Test.py
@@ -17,6 +17,33 @@ def testMenu():
 	PoPy.main()
 	PoPy.main()
 
+# tests each of the cheatcodes
+def testCheatcodes():
+	pass
+
+# tests look, listen, and actions which don't alter the world state
+def testInfo():
+	pass
+
+# tests all forms of navigation
+def testNavigation():
+	pass
+
+# tests open, close, take/grab, put, and drop actions
+def testInventory():
+	pass
+
+# tests use, eat, equip, lock, pour and related actions
+def testBasicItems():
+	pass
+
+# tests attacking and breaking
+def testCombat():
+	pass
+
+# tests basic spell usage
+def testSpells():
+	pass
 
 if __name__ == "__main__":
 	testMenu()
